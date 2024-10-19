82756360fb256b08dde4e881fc8cc2204f43d7b5
zhouxinyu <3135986781@qq.com>
2024-10-20 00:18:57 +0800

    第三次提交

--- repetition.py
+++ repetition.py
@@ -2,6 +2,7 @@ import os
 import obspy
 from obspy.core.stream import Stream
 import repetition_function as rf
+import matplotlib.pyplot as plt
 from pythonProject.repetition_function import match_dataname
 
 #这里是方便后续导入到Path下全部txt文件名称，方便批量化处理服务的。
@@ -9,12 +10,15 @@ if __name__ == "__main__":
     path = os.path.abspath(r"./paz_respond")
     paz = rf.get_paz(path)
     st = Stream()
-    #匹配台站参数和数据：
-    for sta_info,station in paz:
-        try:
-            st += match_dataname(station,sta_info)   #一定要在这一步加入的时候之前就将台站参数导进去，要不然就不好导入了
-        except:
-            continue
-    for i in range(len(st)):
-        print("*"*80)
-        print(st[i].stats)
+    # #匹配台站参数和数据：
+    # for sta_info,station in paz:
+    #     try:
+    #         st += match_dataname(station,sta_info)   #一定要在这一步加入的时候之前就将台站参数导进去，要不然就不好导入了
+    #     except:
+    #         continue
+    # st.simulate(paz_remove="self")    ############这里我们去除台站相应参数失败了，图像很怪#############
+    #   对读取到的数据进行滤波区分：
+    st += rf.load_data(os.path.abspath(r"./data"))#可以在这里更改传入路径
+    st_band = st.copy()
+    st_band.filter('bandpass', freqmin=0.5, freqmax=1.0, corners=2, zerophase=True)
+    #   对于顺利读取滤波的数据，进行section图绘制
--- repetition_function.py
+++ repetition_function.py
@@ -40,10 +40,15 @@ def cut_pazvalue(lines):
     sensitivity_match = sensitivity_pattern.search(lines)
     sensitivity = float(sensitivity_match.group(1))
 
+    # 提取 CONSTANT 值
+    constant_pattern = re.compile(r'CONSTANT\s+(\S+)')
+    constant_match = constant_pattern.search(lines)
+    constant = float(constant_match.group(1))
+
     #提取POLES和ZEROS值
     zeros = cut_pz_values('ZEROS',lines)
     poles = cut_pz_values('POLES',lines)
-    return sensitivity,zeros,poles
+    return sensitivity,constant,zeros,poles
 
 #匹配文件名，方便我们打开对应的data数据文件
 def match_dataname(station,lines):
@@ -55,14 +60,27 @@ def match_dataname(station,lines):
         if dataname_pattern.match(file):
             st = obspy.read(file)
             #要在这里对数据进行进一步处理，加入台站信息
-            input_paz(st,lines)
+            input_paz(st,lines) #   这里我们去掉台站响应参数失败了，为什么
     os.chdir(r"..")
     return st   #传出的是一个数据流
 
-# #这个函数专门用来添加仪器参数
+#这个函数专门用来添加仪器参数
 def input_paz(st,lines):
-    sensitivity, zeros, poles = cut_pazvalue(lines)
+    sensitivity, constant, zeros, poles = cut_pazvalue(lines)
     st[0].stats.paz = AttribDict({'poles': poles,
                                   'zeros': zeros,
-                                  'sensitivity': sensitivity,
-                                  'gain': 1.0}) #==注意==：这里的gain不知道什么意思
+                                  'sensitivity': constant,
+                                  'gain': 1}) #==注意==：这里的gain不知道什么意思
+
+# 这里我们写一个可以一口气读完文件夹所有地震数据的代码，同时之传出Z轴分量：
+#这里我们需要接收所要读取文件的路径
+def load_data(path):
+    for path_state in os.walk(path):
+        path_file = path_state[2]
+    st = Stream()
+    for data_file in path_file:
+        data_filename = path + "\\" + data_file
+        st+=obspy.read(data_filename)
+    #这里可以选择读取输出的方向：
+    st = st.select(component="Z")
+    return st
